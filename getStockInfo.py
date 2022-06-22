97895bac440d4c997e7007456c87861b1e35694b
john_chen <john_chen@phison.com>
2022-06-22 19:07:51 +0800

    Feat: Add get real time price function

--- getStockInfo.py
+++ getStockInfo.py
@@ -8,13 +8,22 @@ def GetSpecifyStock():
         stocks = f.readlines()
     return stocks
 
+def GetRealTimePrice(stock):
+    return stock.get('realtime')['latest_trade_price']
+
 def Run():
     stocks = GetSpecifyStock()
-    for stock in stocks:
-        stock = stock.split("\n")[0].strip()
-        stock_info = twstock.Stock(stock)
-        stock_price = stock_info.price[-5:]
-        print(stock_price)
+    for code in stocks:
+        code = code.split("\n")[0].strip()
+        # Parser realtime info
+        stock_info = twstock.realtime.get(code)
+        # Get real time price
+        latest_trade_price = GetRealTimePrice(stock_info)
+        print("Stock symbol : %s, Latest_trade_price : %s"%(code, latest_trade_price))
+
+        # stock_info = twstock.Stock(stock)
+        # stock_price = stock_info.price[-5:]
+        # print(stock_price)
 
 
 if __name__ == "__main__" :
